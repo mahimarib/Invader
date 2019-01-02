3ade8d9619bfcaf7cd27ec998c45b5cedc56e95a
Mahim <mahim216@gmail.com>
2019-01-02 00:13:02 -0500

    updated gradle to work with java 8 and 11

--- src/org/usfirst/frc/team4571/robot/subsystems/pid/TurnOutput.kt
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team4571.robot.subsystems.pid
-
-import edu.wpi.first.wpilibj.PIDOutput
-import edu.wpi.first.wpilibj.drive.DifferentialDrive
-
-class TurnOutput(private val differentialDrive: DifferentialDrive) : PIDOutput {
-
-    override fun pidWrite(output: Double) = differentialDrive.tankDrive(output, -output)
-}
