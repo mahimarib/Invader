efbd8d0a3a5f07650aa234de82b422d4c2a265c1
Mahim <mahim216@gmail.com>
2019-01-03 20:31:43 -0500

    moving to hardware

--- src/org/usfirst/frc/team4571/robot/hardware/ElevatorEncoder.kt
+++ src/org/usfirst/frc/team4571/robot/hardware/ElevatorEncoder.kt
@@ -1,11 +1,9 @@
-package org.usfirst.frc.team4571.robot.subsystems.pid
-
-import org.usfirst.frc.team4571.robot.RobotMap
+package org.usfirst.frc.team4571.robot.hardware
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX
-
 import edu.wpi.first.wpilibj.PIDSource
 import edu.wpi.first.wpilibj.PIDSourceType
+import org.usfirst.frc.team4571.robot.RobotMap
 
 class ElevatorEncoder(private val elevatorMotor: WPI_TalonSRX) : PIDSource {
 
@@ -13,7 +11,8 @@ class ElevatorEncoder(private val elevatorMotor: WPI_TalonSRX) : PIDSource {
 
     override fun getPIDSourceType(): PIDSourceType = PIDSourceType.kDisplacement
 
-    override fun pidGet(): Double = (-elevatorMotor.getSelectedSensorPosition(0)).toDouble()
+    override fun pidGet(): Double =
+        (-elevatorMotor.getSelectedSensorPosition(0)).toDouble()
 
     fun reset() {
         elevatorMotor.setSelectedSensorPosition(0, 0, RobotMap.PERIOD_IN_MS)
