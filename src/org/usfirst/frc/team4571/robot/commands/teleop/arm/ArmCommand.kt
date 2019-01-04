8ed93415603643eeecad4fdb7c31a787c6c53d56
Mahim <mahim216@gmail.com>
2019-01-03 23:12:08 -0500

    Update ArmCommand.kt

--- src/org/usfirst/frc/team4571/robot/commands/teleop/arm/ArmCommand.kt
+++ src/org/usfirst/frc/team4571/robot/commands/teleop/arm/ArmCommand.kt
@@ -5,10 +5,8 @@ import org.usfirst.frc.team4571.robot.Robot
 import org.usfirst.frc.team4571.robot.subsystems.ArmSystem
 
 object ArmCommand : Command() {
-    private object CustomSpeed {
-        const val OUTTAKE_SPEED = 0.50
-        const val LOWER_SPEED = 0.3
-    }
+    const val OUTTAKE_SPEED = 0.50
+    const val LOWER_SPEED = 0.3
 
     init {
         requires(ArmSystem)
@@ -18,14 +16,10 @@ object ArmCommand : Command() {
         val rightTrigger = Robot.GAMEPAD.rightTrigger
         val leftTrigger = Robot.GAMEPAD.leftTrigger
         when {
-            rightTrigger > 0 && rightTrigger > leftTrigger ->
-                ArmSystem.setArmMotors(-rightTrigger)
-            leftTrigger > 0 && leftTrigger > rightTrigger ->
-                ArmSystem.setArmMotors(leftTrigger)
-            Robot.GAMEPAD.buttonB.get() ->
-                ArmSystem.setArmMotors(CustomSpeed.OUTTAKE_SPEED)
-            Robot.GAMEPAD.buttonA.get() ->
-                ArmSystem.setArmMotors(CustomSpeed.LOWER_SPEED)
+            rightTrigger > 0 && rightTrigger > leftTrigger -> ArmSystem.setArmMotors(-rightTrigger)
+            leftTrigger > 0 && leftTrigger > rightTrigger -> ArmSystem.setArmMotors(leftTrigger)
+            Robot.GAMEPAD.buttonB.get() -> ArmSystem.setArmMotors(OUTTAKE_SPEED)
+            Robot.GAMEPAD.buttonA.get() -> ArmSystem.setArmMotors(LOWER_SPEED)
             else -> ArmSystem.stop()
         }
     }
