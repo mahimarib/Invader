b0592b4c0209fe7068a2f49922f1eec1977045ca
Mahim <mahim216@gmail.com>
2019-01-03 23:09:55 -0500

    removed explicit typing

--- src/org/usfirst/frc/team4571/robot/DriveStick.kt
+++ src/org/usfirst/frc/team4571/robot/DriveStick.kt
@@ -4,18 +4,18 @@ import edu.wpi.first.wpilibj.Joystick
 import edu.wpi.first.wpilibj.buttons.JoystickButton
 
 class DriveStick(port: Int) : Joystick(port) {
-    val button1: JoystickButton = JoystickButton(this, 1)
-    val button2: JoystickButton = JoystickButton(this, 2)
-    val button3: JoystickButton = JoystickButton(this, 3)
-    val button4: JoystickButton = JoystickButton(this, 4)
-    val button5: JoystickButton = JoystickButton(this, 5)
-    val button6: JoystickButton = JoystickButton(this, 6)
-    val button7: JoystickButton = JoystickButton(this, 7)
-    val button8: JoystickButton = JoystickButton(this, 8)
-    val button9: JoystickButton = JoystickButton(this, 9)
-    val button10: JoystickButton = JoystickButton(this, 10)
-    val button11: JoystickButton = JoystickButton(this, 11)
-    val button12: JoystickButton = JoystickButton(this, 12)
+    val button1 = JoystickButton(this, 1)
+    val button2 = JoystickButton(this, 2)
+    val button3 = JoystickButton(this, 3)
+    val button4 = JoystickButton(this, 4)
+    val button5 = JoystickButton(this, 5)
+    val button6 = JoystickButton(this, 6)
+    val button7 = JoystickButton(this, 7)
+    val button8 = JoystickButton(this, 8)
+    val button9 = JoystickButton(this, 9)
+    val button10 = JoystickButton(this, 10)
+    val button11 = JoystickButton(this, 11)
+    val button12 = JoystickButton(this, 12)
 
     val xAxis: Double
         get() = getRawAxis(0)
--- src/org/usfirst/frc/team4571/robot/Gamepad.kt
+++ src/org/usfirst/frc/team4571/robot/Gamepad.kt
@@ -4,36 +4,36 @@ import edu.wpi.first.wpilibj.Joystick
 import edu.wpi.first.wpilibj.buttons.JoystickButton
 
 class Gamepad(port: Int) : Joystick(port) {
-    val buttonA: JoystickButton = JoystickButton(this, 1)
-    val buttonB: JoystickButton = JoystickButton(this, 2)
-    val buttonX: JoystickButton = JoystickButton(this, 3)
-    val buttonY: JoystickButton = JoystickButton(this, 4)
-    val leftBumper: JoystickButton = JoystickButton(this, 5)
-    val rightBumper: JoystickButton = JoystickButton(this, 6)
-    val backButton: JoystickButton = JoystickButton(this, 7)
-    val startButton: JoystickButton = JoystickButton(this, 8)
-    val leftJoystickButton: JoystickButton = JoystickButton(this, 9)
-    val rightJoystickButton: JoystickButton = JoystickButton(this, 10)
+    val buttonA = JoystickButton(this, 1)
+    val buttonB = JoystickButton(this, 2)
+    val buttonX = JoystickButton(this, 3)
+    val buttonY = JoystickButton(this, 4)
+    val leftBumper = JoystickButton(this, 5)
+    val rightBumper = JoystickButton(this, 6)
+    val backButton = JoystickButton(this, 7)
+    val startButton = JoystickButton(this, 8)
+    val leftJoystickButton = JoystickButton(this, 9)
+    val rightJoystickButton = JoystickButton(this, 10)
 
     val leftXAxis: Double
-        get() = this.getRawAxis(0)
+        get() = getRawAxis(0)
 
     // negative because controls are reversed
     val leftYAxis: Double
-        get() = -this.getRawAxis(1)
+        get() = -getRawAxis(1)
 
     val rightXAxis: Double
-        get() = this.getRawAxis(4)
+        get() = getRawAxis(4)
 
     // negative because controls are reversed
     val rightYAxis: Double
-        get() = -this.getRawAxis(5)
+        get() = -getRawAxis(5)
 
     val leftTrigger: Double
-        get() = this.getRawAxis(2)
+        get() = getRawAxis(2)
 
     val rightTrigger: Double
-        get() = this.getRawAxis(3)
+        get() = getRawAxis(3)
 
     val isDPadPressedUp: Boolean
         get() = pov == 0
